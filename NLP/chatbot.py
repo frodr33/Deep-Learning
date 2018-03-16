cf2ce8a868d299aac49e3ea43855f883251d8abd
frodr33 <fsr32@cornell.edu>
2018-03-15 21:54:27 -0400

    Added some preprocessing steps

--- NLP/chatbot.py
+++ NLP/chatbot.py
@@ -13,3 +13,16 @@ import time
 lines = open("movie_lines.txt", encoding = "utf-8", errors = "ignore").read().split('\n')
 conversations = open("movie_conversations.txt", encoding = "utf-8", errors = "ignore").read().split('\n')
 
+# Create Dictionary to map each line with ID
+# Easiest way to keep track of inputs and outputs
+id2line = {}
+for line in lines:
+    _line = line.split(" +++$+++ ")
+    if len(_line) == 5:
+        id2line[_line[0]] = _line[4]
+        
+# Creating a list of all the conversations
+conversations_ids = []
+for conv in conversations[:-1]:
+    _conv = conv.split( " +++$+++ ")[-1][1:-1].replace("'", "").replace(" ", "")
+    conversations_ids.append(_conv.split(","))
