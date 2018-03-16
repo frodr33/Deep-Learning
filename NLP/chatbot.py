38bc92199de85db408331be3d45592c7663b191e
frodr33 <fsr32@cornell.edu>
2018-03-15 22:28:51 -0400

    Started Cleaning Q/As

--- NLP/chatbot.py
+++ NLP/chatbot.py
@@ -36,3 +36,39 @@ for conversation in conversations_ids:
     for i in range(len(conversation) -1):
         questions.append(id2line[conversation[i]])
         answers.append(id2line[conversation[i+1]])
+        
+# Doing a first cleaning of the texts
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"\'ll", "will", text)
+    text = re.sub(r"\'ve", "have", text)
+    text = re.sub(r"\'re", "are", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "cannot", text)
+    text = re.sub(r"[-()\"/@;:<>{}+=~|.?,]", "cannot", text)
+    return text
+    
+# Clean Questions
+clean_questions = []
+for question in questions:
+    clean_questions.append(clean_text(question))
+    
+# Clean Answers..todo
+clean_questions = []
+for question in questions:
+    clean_questions.append(clean_text(question))
+    
+    
+    
+    
+    
+    
+    
+    
+    
