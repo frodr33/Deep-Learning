6d4e34cc98e4c21a919039ab91bb90c3214dd7cc
frodr33 <fsr32@cornell.edu>
2018-03-15 21:23:21 -0400

    imported data set

--- NLP/chatbot.py
+++ NLP/chatbot.py
@@ -6,3 +6,10 @@ import tensorflow as tf
 import re
 import time
 
+
+########## Data Preprocessing ############
+
+# Import Data Set
+lines = open("movie_lines.txt", encoding = "utf-8", errors = "ignore").read().split('\n')
+conversations = open("movie_conversations.txt", encoding = "utf-8", errors = "ignore").read().split('\n')
+
